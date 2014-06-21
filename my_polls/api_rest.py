e37ff11df5e291aef6e35222255b46e3acdf920b
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-06-21 16:58:22 -0300

    usando mapeamento seletivo dos modelos do BD

--- my_polls/api_rest.py
+++ my_polls/api_rest.py
@@ -9,7 +9,27 @@ DB_PATH = os.path.join(APP_PATH, 'db.sqlite')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + DB_PATH
 
-from sandman.model import activate
+
+from sandman.model import register, activate, Model
+
+
+class Poll(Model):
+    __tablename__ = 'poll_poll'
+
+
+class Vote(Model):
+    __tablename__ = 'poll_vote'
+
+
+class Item(Model):
+    __tablename__ = 'poll_item'
+
+
+class User(Model):
+    __tablename__ = 'auth_user'
+
+# # register can be called with an iterable or a single class
+register((Poll, Vote, Item, User))
 
 activate(browser=False)
 
